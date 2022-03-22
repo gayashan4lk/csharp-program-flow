4f9de333528f78754c180b4fca4be69d2e7f2bcd
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-22 22:42:11 +0530

    Expression Statements

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -6,11 +6,14 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
+            // Calculated Values
             double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
             double unasweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
+            double averallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
 
             Console.WriteLine($"Response Percentage : { responseRate }");
             Console.WriteLine($"Unanswered Surveys : { unasweredCount }");
+            Console.WriteLine($"Overall Score : { averallScore }");
         }
     }
 }
